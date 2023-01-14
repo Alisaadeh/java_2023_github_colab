028f61708adf4b449554c448cd6849fc0cceebbe
User <as9136991@gmail.com>
2023-01-14 23:33:03 +0200

    added the properties of the Bus

--- java_2023_github_colab/src/Bus.java
+++ java_2023_github_colab/src/Bus.java
@@ -1,2 +1,30 @@
 public class Bus {
+
+    private String busName;
+    private Integer busNumber;
+    private String driverName;
+
+    public String getBusName() {
+        return busName;
+    }
+
+    public void setBusName(String busName) {
+        this.busName = busName;
+    }
+
+    public Integer getBusNumber() {
+        return busNumber;
+    }
+
+    public void setBusNumber(Integer busNumber) {
+        this.busNumber = busNumber;
+    }
+
+    public String getDriverName() {
+        return driverName;
+    }
+
+    public void setDriverName(String driverName) {
+        this.driverName = driverName;
+    }
 }
